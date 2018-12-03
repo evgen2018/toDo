8b58d58bec377aadc6956373dc40731323327677
yujin4 <timofeev111@ukr.net>
2018-12-03 18:25:58 +0200

    Updates

---
+++ precache-manifest.5c93e93c4e2b70a6df445c0d5299b07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd481ec6de5e7e7b5401",
+    "url": "/toDo/static/js/runtime~main.bd481ec6.js"
+  },
+  {
+    "revision": "f027e9f5bd220175c01b",
+    "url": "/toDo/static/js/main.f027e9f5.chunk.js"
+  },
+  {
+    "revision": "a613bc9b3b75faa17f42",
+    "url": "/toDo/static/js/1.a613bc9b.chunk.js"
+  },
+  {
+    "revision": "f027e9f5bd220175c01b",
+    "url": "/toDo/static/css/main.0dbca481.chunk.css"
+  },
+  {
+    "revision": "5737f42435a727c7a74ddfefe95bad1b",
+    "url": "/toDo/index.html"
+  }
+];
